1e0147552af98fa1a801f76cfd777bccef6f6806
lyesdki <lyes.douki@alumni.univ-avignon.fr>
2024-01-31 15:17:58 +0100

    5 eme question, method Post

--- src/main/java/com/ceri/servelet/HelloServlet.java
+++ src/main/java/com/ceri/servelet/HelloServlet.java
@@ -11,11 +11,24 @@ public class HelloServlet extends HttpServlet {
             throws IOException {
 
         PrintWriter ou = response.getWriter();
+        //question 3
         String parameter = request.getParameter("name");
         if(parameter == null)
             ou.println("hello world from servlet, no parameter yet");
         else
             ou.println("hello world from servlet, your name is : " + parameter);
     }
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        PrintWriter ou = response.getWriter();
+        ////question 4
+        String parameterNom = request.getParameter("nom");
+        String parameterPrenom = request.getParameter("prenom");
+        if(parameterNom == null && parameterPrenom == null)
+            ou.println("hello world from servlet, no parameter yet");
+        else
+            ou.println("hello world from servlet, POST method, ton nom : " + parameterNom + " et ton prénom : " + parameterPrenom);
+    }
+
 
 }
